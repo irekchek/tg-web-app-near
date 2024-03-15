9c5a0ccb997b18677c4622883f05dd04d2ebdf0e
irekchek <irekgaliev.official@gmail.com>
2024-03-15 14:38:46 +0300

    +header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="left">
+          <img  src="/src/img/logo.jpg" alt="logo icon" className="icon" />
+        </div>
+        <div className="center">
+          <button className="button shrimp-button">
+            Shrimp
+            <span className="icon">🦐</span>
+          </button>
+        </div>
+        <div className="right">
+          <button className="button qr-code-button">
+            <img src="img/qr_icon.jpg" alt="QR Code Icon" className="icon" />
+          </button>
+          <button className="button settings-button">
+            <img src="/src/img/setting.png" alt="Settings Icon" className="icon" />
+          </button>
+        </div>
       </header>
+
+
     </div>
   );
 }
